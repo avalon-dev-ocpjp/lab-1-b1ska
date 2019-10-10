6195a8713f120b6f1bb20ef082dcb1bf2534d3d3
Andrey S <48486192+b1ska@users.noreply.github.com>
2019-10-10 19:38:30 +0300

    Add files via upload
    Не совсем понял эту лабораторку...она и так была готова, с параметрами запускается\работает.

--- src/ru/avalon/java/ocpjp/labs/tasks/objects/RandomCitizen.java
+++ src/ru/avalon/java/ocpjp/labs/tasks/objects/RandomCitizen.java
@@ -22,9 +22,9 @@ public class RandomCitizen implements Factory<Citizen>, Citizen {
     private List<String> countries;
     private List<String> names;
     private List<String> lastNames;
-    private String pathCountry = "/Users/lesya/NetBeansProjects/lab-1/src/ru/avalon/java/ocpjp/labs/resources/countries.txt";
-    private String pathName = "/Users/lesya/NetBeansProjects/lab-1/src/ru/avalon/java/ocpjp/labs/resources/first-names.txt";
-    private String pathLastName = "/Users/lesya/NetBeansProjects/lab-1/src/ru/avalon/java/ocpjp/labs/resources/last-names.txt";
+    private String pathCountry = "D:\\dd\\resources\\countries.txt";
+    private String pathName = "D:\\dd\\resources\\first-names.txt";
+    private String pathLastName = "D:\\dd\\resources\\last-names.txt";
     private String code;
     private String countryName;
     private Country country;
